298b81b82b675370b77cfbcce2f5f1d129a92754
Samarth Bhamare <77913972+Samarth0211@users.noreply.github.com>
2023-10-27 23:44:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import sklearn
+import scikit-learn
 
 teams = ['Mumbai Indians', 'Royal Challengers Bangalore',
        'Lucknow Super Giants', 'Kolkata Knight Riders',
